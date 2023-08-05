57f6f798af614ce2695f580ab0be039636168c02
arnold004 <arnoldpascal01@gmail.com>
2023-08-05 22:05:56 +0530

    CommitDay3

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -29,9 +29,17 @@ class _HomeState extends State<Home> {
     Map<String, dynamic> jsonData = jsonDecode(response.body);
     jsonData["photos"].forEach((element) {
       // print(element);
-      WallpaperModel wallpaperModel = new WallpaperModel();
-      wallpaperModel = jsonData[""];
+      WallpaperModel wallpaperModel = new WallpaperModel(
+          // Changed Wallpaper Model
+          src: SrcModel.fromMap(element["src"]),
+          photographer_url: element["photographer_url"],
+          photographer_id: element["photographer_id"],
+          photographer: element["photographer"]);
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpapers.add(wallpaperModel);
     });
+
+    setState(() {});
   }
 
   @override
@@ -49,50 +57,54 @@ class _HomeState extends State<Home> {
         title: brandName(),
         elevation: 0.0,
       ),
-      body: Container(
-          child: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: Color(0xfff5f8fd),
-                borderRadius: BorderRadius.circular(30)),
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            margin: EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Search Wallpaper", border: InputBorder.none),
+      body: SingleChildScrollView(
+        child: Container(
+            child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xfff5f8fd),
+                  borderRadius: BorderRadius.circular(30)),
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search Wallpaper",
+                          border: InputBorder.none),
+                    ),
                   ),
-                ),
-                Icon(Icons.search)
-              ],
+                  Icon(Icons.search)
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(50, 15, 50, 5),
-            child: credit(),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Container(
-            height: 80,
-            child: ListView.builder(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                itemCount: categories.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return CategoriesTile(
-                    title: categories[index].categorieName,
-                    imgUrl: categories[index].imgUrl,
-                  );
-                }),
-          )
-        ],
-      )),
+            Container(
+              margin: EdgeInsets.fromLTRB(50, 15, 50, 5),
+              child: credit(),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  itemCount: categories.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return CategoriesTile(
+                      title: categories[index].categorieName,
+                      imgUrl: categories[index].imgUrl,
+                    );
+                  }),
+            ),
+            wallpapersList(wallpapers: wallpapers, context: context)
+          ],
+        )),
+      ),
     );
   }
 }
